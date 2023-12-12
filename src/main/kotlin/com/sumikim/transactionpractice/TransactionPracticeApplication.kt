2e84c658f82aeab35525a3ddbd3403a0404fca49
김수미 서비스개발팀 <sumikim@sk.com>
2023-12-12 11:47:31 +0900

    :tada: 프로젝트 생성

---
+++ src/main/kotlin/com/sumikim/transactionpractice/TransactionPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.sumikim.transactionpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransactionPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TransactionPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/sumikim/transactionpractice/TransactionPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sumikim.transactionpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransactionPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
